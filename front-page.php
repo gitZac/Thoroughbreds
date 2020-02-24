7c1e839bed25be6681264a44b29811f80a5f7c48
Zac <ztaylor555@gmail.com>
2020-02-23 22:33:26 -0500

    Started work on front page // added callout module // added highlight module which still needs some work

--- front-page.php
+++ front-page.php
@@ -8,60 +8,77 @@
 get_header();
 ?>
 
-
-
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
         <?php do_action('thoroughbreds_homepage'); ?>
 
-        <div class="row">
-            
-            <?php get_sidebar('left'); ?>
-
-            <div class="homepage-page-content col-sm-<?php echo thoroughbreds_main_width(); ?>">
-                
-                <?php if (have_posts()) : ?>
-
-                    <?php if (is_home() && !is_front_page()) : ?>
-                        <header>
-                            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                        </header>
-                    <?php endif; ?>
-
-                    <?php $front = get_option('show_on_front'); ?>
-
-                    <?php echo $front == 'posts' ? '<div class="thoroughbreds-blog-content">' : ''; ?>
-
-                    <?php while (have_posts()) : the_post(); ?>
-
-                        <?php
-                        if ('posts' == get_option('show_on_front')) :
-                            get_template_part('template-parts/content-blog', get_post_format());
-                        else:
-                            get_template_part('template-parts/content-page-home', get_post_format());
-                        endif;
-                        ?>
-
-                    <?php endwhile; ?>
-                    <?php echo $front == 'posts' ? '</div>' : ''; ?>
-                    <div class="thoroughbreds-pagination">
-                        <?php echo paginate_links(); ?>
+        <div class="container-fluid bg-gray-light">
+            <section class="section callouts ">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="section-title">
+                            <h2 class="section__title--title">Shows and Events</h2>
+                        </div><!-- /.section-title-->
                     </div>
+                    <div class="col-sm-6">
+                        <div class="callouts--card">
+                            <div class="callouts--card--image" style="background-image:url(https://thethoroughbreds.dev.cc/wp-content/uploads/2020/02/mainstreet-1.jpg);">
+                            </div>
+                            <div class="callouts--card--content">
+                            <h4 class="callouts--card--title">Christmas with the Thoroughbreds</h4>
+                            <div class="callouts--card--dates">Through February 17th</div>
+                            <p class="callouts--card--description">A Caribbean-infused musical fairy tale about love’s extraordinary power to tear down walls and transform our lives.</p>
+                            <div class="callouts--card--link">
+                                <a href="#" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3">Get tickets!</a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="callouts--card">
+                            <div class="callouts--card--image" style="background-image:url(https://thethoroughbreds.dev.cc/wp-content/uploads/2020/02/drew-1.jpg);">
+                            </div>
+                            <div class="callouts--card--content">
+                                <h4 class="callouts--card--title">Winter Song Workshop</h4>
+                                <div class="callouts--card--dates">March 24th</div>
+                                <p class="callouts--card--description">Our winter music workshop is here! If you're looking to brush up on your music technique, now's your chance!</p>
+                                <div class="callouts--card--link">
+                                    <a href="#" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3">Get tickets!</a>
+                                </div> <!-- /.card--link -->
+                            </div> <!-- /.card--content -->
+                        </div> <!-- /.callouts--card -->
+                    </div> <!-- /.col-sm6 -->
+                </div> <!-- /.row -->
+            </section> <!-- /.Callouts -->
+        </div> <!-- /.container-fluid-->
+        <div class="">
+            <div class="section highlights">
+                <div class="row">
+                    <div class="section-title">
+                        <h2 class="section__title--title">Hire Us!</h2>
+                    </div><!-- /.section-title-->
+                </div>
+                <div class="row row--full-width">
+                </div>
+                <div class="highlights--list">
+                    <div class="highlights--list--item">
+                        <a href="#" class="highlights--card">
+                            <div style="background-image:url(https://thethoroughbreds.dev.cc/wp-content/uploads/2020/02/west1.jpg);" class="highlights--card--image"></div>
+                            <div class="highlights--card--shade"></div>
+                            <div class="highlights--card--content">
+                                <strong class="highlights--card--title">Chorus Show Package</strong>
+                                <em class="highlights--card--subtitle">Perfect for events, galas and services.</em>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-                <?php else : ?>
 
-                    <?php get_template_part('template-parts/content', 'none'); ?>
-
-                <?php endif; ?>
-
-            </div>
+            </div><!-- /.section.highlights-->
+        </div><!-- /.container-fluid-->
 
 
-            <?php get_sidebar(); ?>
-
-
-        </div>
     </main><!-- #main -->
 </div><!-- #primary -->
 
