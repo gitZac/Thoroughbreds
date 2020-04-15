45159686bc6509046a5bc299863ac63844164347
Zac <ztaylor555@gmail.com>
2020-04-15 10:19:30 -0400

    Added page template for events page // modified legacy css to correct margin

--- page-events.php
+++ page-events.php
@@ -19,22 +19,12 @@ get_header();
 
         <?php while (have_posts()) : the_post(); ?>
 
-            <?php get_template_part('template-parts/content', 'page'); ?>
-
-            <?php
-            // If comments are open or we have at least one comment, load up the comment template.
-            if (comments_open() || get_comments_number()) :
-                comments_template();
-            endif;
-            ?>
+            <?php get_template_part('template-parts/content', 'page_events'); ?>
 
         <?php endwhile; // End of the loop. ?>
 
     </main><!-- #main -->
 
-
-    
-    
 </div><!-- #primary -->
 
 
