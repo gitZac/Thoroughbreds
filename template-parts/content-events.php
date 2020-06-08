feeeb55c8a5a2f644ff379ea203ea383f0de8050
Zac <ztaylor555@gmail.com>
2020-06-08 18:59:24 -0400

    Refactoring hero

--- template-parts/content-events.php
+++ template-parts/content-events.php
@@ -8,28 +8,9 @@
  */
 ?>
 
-<?php if (get_post_thumbnail_id($post->ID)) : ?>
-
-    <section class="hero">
-        <div class="hero__image" style="background-image:url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)) ?>);"></div>
-        <div class="hero__shade"></div>
-        <div class="hero__content">
-            <div class="hero__title ">
-                <h1 class="margin-0"><?php the_title(); ?></h1>
-            </div>
-            <div class="hero__subtitle">
-                <?php the_field('event_date'); ?>
-            </div>
-            <div class="hero__cta">
-                <a href="<?php the_permalink(); ?>" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3">Get Tickets</a>
-            </div>
-        </div>
-    </section>
-<?php endif; ?>
+<?php get_template_part('template-parts/modules/hero-inner.php'); ?>
 
 <div class="row">
-    
-    <?php get_sidebar('left'); ?>
     
     <div class="col-sm-<?php echo thoroughbreds_main_width(); ?>">
 
@@ -75,6 +56,6 @@
 
     </div>
     
-    <?php get_sidebar(); ?>
+    <?php //get_sidebar(); ?>
 
 </div>
--- template-parts/modules/hero-inner.php
+++ template-parts/modules/hero-inner.php
@@ -1,16 +1,17 @@
-<section class="hero">
-    <div class="hero__image" style="background-image:url(<?php echo $image['url']; ?>);"></div>
-    <div class="hero__shade"></div>
-    <div class="hero__content">
-        <div class="hero__title ">
-            <h1 class="margin-0"><?php single_term_title(); ?></h1>
+<?php if (get_post_thumbnail_id($post->ID)) : ?>
+    <section class="hero">
+        <div class="hero__image" style="background-image:url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)) ?>);"></div>
+        <div class="hero__shade"></div>
+        <div class="hero__content">
+            <div class="hero__title ">
+                <h1 class="margin-0"><?php the_title(); ?></h1>
+            </div>
+            <div class="hero__subtitle">
+                <?php the_field('event_date'); ?>
+            </div>
+            <div class="hero__cta">
+                <a href="<?php the_permalink(); ?>" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3">Get Tickets</a>
+            </div>
         </div>
-        <div class="hero__subtitle">
-            <?php //the_field('event_date'); ?>
-        </div>
-        <div class="hero__cta">
-            <a href="<?php $current_url; ?>" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3">Book <?php single_term_title(); ?></a>
-            <a href="#" class="thoroughbreds-button secondary small animated flipInX slide2_button1 delay3">Our Next Performance</a>
-        </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
