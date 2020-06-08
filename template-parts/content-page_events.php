014a7a441e87e09d8df2b12a53a66dab9d385224
Zac <ztaylor555@gmail.com>
2020-06-08 18:37:29 -0400

    Fixed shows and events hero

--- template-parts/content-page_events.php
+++ template-parts/content-page_events.php
@@ -8,7 +8,20 @@
  */
 ?>
 <div class="container-fluid">
-    <?php get_template_part('template-parts/modules/hero-inner'); ?>
+    <?php if (get_post_thumbnail_id($post->ID)) : ?>
+    <section class="hero">
+        <div class="hero__image" style="background-image:url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)) ?>);"></div>
+        <div class="hero__shade"></div>
+        <div class="hero__content">
+            <div class="hero__title ">
+                <h1 class="margin-0"><?php the_title(); ?></h1>
+            </div>
+            <div class="hero__subtitle">
+                <?php //the_field('event_date'); ?>
+            </div>
+        </div>
+    </section>
+    <?php endif; ?>
 </div>
 <div class="container-fluid bg-gray-light">
     <section class="section callouts ">
--- template-parts/modules/hero-inner.php
+++ template-parts/modules/hero-inner.php
@@ -1,13 +1,16 @@
-
 <section class="hero">
-    <div class="hero__image" style="background-image:url(<?php if (get_post_thumbnail_id($post->ID) ) : echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); endif; ?>);"></div>
+    <div class="hero__image" style="background-image:url(<?php echo $image['url']; ?>);"></div>
     <div class="hero__shade"></div>
     <div class="hero__content">
         <div class="hero__title ">
-            <h1 class="margin-0"><?php the_title(); ?></h1>
+            <h1 class="margin-0"><?php single_term_title(); ?></h1>
         </div>
         <div class="hero__subtitle">
             <?php //the_field('event_date'); ?>
+        </div>
+        <div class="hero__cta">
+            <a href="<?php $current_url; ?>" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3">Book <?php single_term_title(); ?></a>
+            <a href="#" class="thoroughbreds-button secondary small animated flipInX slide2_button1 delay3">Our Next Performance</a>
         </div>
     </div>
 </section>
