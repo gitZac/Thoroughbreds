bed35d14c149a8883f6a906821535df25eca081e
Zac <ztaylor555@gmail.com>
2020-06-14 20:52:48 -0400

    Fixed page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -23,60 +23,33 @@
             </div>
         </div>
     </section>
+
 <?php endif; ?>
 
 </div>
 
-<div class="section">
-    <div class="row">
-        <div class="section-title">
-            <h2 class="section__title--title"><?php the_field('secondary_title');?></h2>
-        </div><!-- /.section-title-->
-    </div>
-</div>
-
-<div class="row">
+<div class="container">
+    <section class="section margin-none">
+        <div class="row">
+            <div class="section-title margin-none">
+                <h2 class="section__title--title-nb"><?php the_field('secondary_title');?></h2>
+            </div><!-- /.section-title-->
+        </div>
+    </section>
     
-    <?php get_sidebar('left'); ?>
-    
-    <div class="col-sm-<?php echo thoroughbreds_main_width(); ?>">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        
-            <div class="entry-content">
+    <div class="entry-content">
 
-                <?php if (!get_post_thumbnail_id($post->ID)) : ?>
+        <?php the_content(); ?>
+        <?php
+        wp_link_pages(array(
+            'before' => '<div class="page-links">' . esc_html__('Pages:', 'thoroughbreds'),
+            'after' => '</div>',
+        ));
+        ?>
+    </div><!-- .entry-content -->
 
-                    <header class="entry-header">
-                        <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-                    </header><!-- .entry-header -->
+    </article><!-- #post-## -->
 
-                <?php endif; ?>
-
-
-                <?php the_content(); ?>
-                <?php
-                wp_link_pages(array(
-                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'thoroughbreds'),
-                    'after' => '</div>',
-                ));
-                ?>
-            </div><!-- .entry-content -->
-
-            <footer class="entry-footer">
-                <?php
-                edit_post_link(
-                        sprintf(
-                                /* translators: %s: Name of current post */
-                                esc_html__('Edit %s', 'thoroughbreds'), the_title('<span class="screen-reader-text">"', '"</span>', false)
-                        ), '<span class="edit-link">', '</span>'
-                );
-                ?>
-            </footer><!-- .entry-footer -->
-
-
-
-        </article><!-- #post-## -->
-    </div>
 </div>
-
