99de651f0f83c0463883a96df34e6d4b9de02c6a
Zac <ztaylor555@gmail.com>
2020-06-14 22:55:50 -0400

    fixed some issues with secondary button and nav colors

--- single.php
+++ single.php
@@ -14,7 +14,7 @@ get_header();
 
         <?php while (have_posts()) : the_post(); ?>
 
-            <?php get_template_part('template-parts/content', 'single'); ?>
+            <?php get_template_part('template-parts/content', 'page'); ?>
 
         <?php endwhile; // End of the loop. ?>
 
